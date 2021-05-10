8f7e1b45f7528564e227cf5d89cc4da3cee33fa9
mininglamp <starchen_jnu@163.com>
2021-05-10 16:38:31 +0800

    test2分支代码

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -4,4 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("test git");
     }
+    public int mutiply(int a, int b){
+        return a * b;
+    }
 }

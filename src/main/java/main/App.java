5116fd130cfa725437d8e9f17bca0fde47d64f1c
mininglamp <starchen_jnu@163.com>
2021-05-10 17:31:44 +0800

    master代码还原

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -4,11 +4,4 @@ public class App {
     public static void main(String[] args) {
         System.out.println("test git");
     }
-
-    public int add(int a,int b){
-        return a+b;
-    }
-    public int mutiply(int a, int b){
-        return a * b;
-    }
 }
